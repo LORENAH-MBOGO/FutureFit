9df29918d382bcf6c3670c05b5c7f9fbad7f5a6f
Lorenah <lorayne911@gmail.com>
2021-01-19 10:13:36 +0300

    add fragment page adapter

--- app/src/main/java/ui/DoctorDetailActivity.java
+++ app/src/main/java/ui/DoctorDetailActivity.java
@@ -1,16 +1,37 @@
 package ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.skylar.futurefit.R;
 
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import models.Doctor;
+
 public class DoctorDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    private DoctorPagerAdapter adapterViewPager;
+    ArrayList<Doctor> mDoctors = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_detail);
+        ButterKnife.bind(this);
+
+        mDoctors = Parcels.unwrap(getIntent().getParcelableExtra("doctors"));
+        int startingPosition = Integer.parseInt(getIntent().getStringExtra("position"));
+
+        adapterViewPager = new DoctorPagerAdapter(getSupportFragmentManager(), mDoctors);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
