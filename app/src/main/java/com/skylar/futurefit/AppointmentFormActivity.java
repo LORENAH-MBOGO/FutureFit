81d0755bfc64deb012b0aa51a572ef255b6c3239
Lorenah <lorayne911@gmail.com>
2021-01-19 05:49:05 +0300

    Add placeholder content to AppontmentsListView

---
+++ app/src/main/java/com/skylar/futurefit/AppointmentFormActivity.java
@@ -0,0 +1,14 @@
+package com.skylar.futurefit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_form);
+    }
+}
