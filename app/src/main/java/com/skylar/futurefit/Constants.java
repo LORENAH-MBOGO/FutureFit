2c18e9946ee43c81a828289af24812829870bf23
Lorenah <lorayne911@gmail.com>
2021-01-19 08:17:13 +0300

    add service and set up

---
+++ app/src/main/java/com/skylar/futurefit/Constants.java
@@ -0,0 +1,9 @@
+package com.skylar.futurefit;
+
+import static com.skylar.futurefit.BuildConfig.*;
+
+public class Constants {
+    public static final String BETTER_DOCTOR_USER_KEY = BuildConfig.BETTER_DOCTOR_USER_KEY;
+    public static final String BETTER_DOCTOR_BASE_URL = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=";
+    public static final String BETTER_DOCTOR_SPECIALTY_QUERY_PARAMETER = "specialty";
+}
