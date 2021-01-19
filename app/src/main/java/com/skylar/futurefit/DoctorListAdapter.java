045e31282361fa791f364b5bc5577b3b8c75de4a
Lorenah <lorayne911@gmail.com>
2021-01-19 09:37:37 +0300

    replace ListView in DoctorActivity with RecyclerView

---
+++ app/src/main/java/com/skylar/futurefit/DoctorListAdapter.java
@@ -0,0 +1,62 @@
+package com.skylar.futurefit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import models.Doctor;
+
+public class DoctorListAdapter extends RecyclerView.Adapter<DoctorListAdapter.DoctorViewHolder>  {
+    private ArrayList<Doctor> mDoctors = new ArrayList<>();
+    private Context mContext;
+
+    public DoctorListAdapter(Context context, ArrayList<Doctor> doctors) {
+        mContext = context;
+        mDoctors = doctors;
+    }
+
+    @Override
+    public DoctorListAdapter.DoctorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.doctor_list_item, parent, false);
+        DoctorViewHolder viewHolder = new DoctorViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DoctorListAdapter.DoctorViewHolder holder, int position) {
+        holder.bindDoctor(mDoctors.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDoctors.size();
+    }
+
+    public class DoctorViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.doctorImageView) ImageView mDoctorImageView;
+        @BindView(R.id.doctorNameTextView) TextView mDoctorNameTextView;
+        @BindView(R.id.specialtyTextView) TextView mSpecialtyTextView;
+
+        private Context mContext;
+
+        public DoctorViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindDoctor(Doctor doctor) {
+            mDoctorNameTextView.setText(doctor.getName());
+            mSpecialtyTextView.setText(doctor.getSpecialty());
+        }
+    }
+}
--- app/src/main/java/ui/DoctorActivity.java
+++ app/src/main/java/ui/DoctorActivity.java
@@ -1,6 +1,8 @@
 package ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +10,7 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.skylar.futurefit.DoctorListAdapter;
 import com.skylar.futurefit.R;
 
 import java.io.IOException;
@@ -22,10 +25,10 @@ import okhttp3.Response;
 import services.BetterDoctorService;
 
 public class DoctorActivity extends AppCompatActivity {
-    @BindView(R.id.listView)
-    ListView mListView;
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
     public ArrayList<Doctor> mDoctors = new ArrayList<>();
-    public static final String TAG = DoctorActivity.class.getSimpleName();
+    private DoctorListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,26 +57,15 @@ public class DoctorActivity extends AppCompatActivity {
                     @Override
                     public void run() {
 
-                        String[] doctorNames = new String[mDoctors.size()];
-                        for (int i = 0; i < doctorNames.length; i++) {
-                            doctorNames[i] = mDoctors.get(i).getName();
-                        }
-
-                        ArrayAdapter adapter = new ArrayAdapter(DoctorActivity.this, android.R.layout.simple_list_item_1, doctorNames);
-                        mListView.setAdapter(adapter);
-
-                        for (Doctor doctor : mDoctors) {
-                            Log.d(TAG, "Name: " + doctor.getName());
-                            Log.d(TAG, "Specialty: " + doctor.getSpecialty());
-                            Log.d(TAG, "Phone: " + doctor.getPhone());
-                            Log.d(TAG, "Lat: " + Double.toString(doctor.getLatitude()));
-                            Log.d(TAG, "Lon: " + Double.toString(doctor.getLongitude()));
-                            Log.d(TAG, "Address: " + doctor.getAddress());
-                            Log.d(TAG, "Bio: " + doctor.getBio());
-                        }
+                        mAdapter = new DoctorListAdapter(getApplicationContext(), mDoctors);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(DoctorActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
                     }
                 });
             }
         });
     }
+}
 }
